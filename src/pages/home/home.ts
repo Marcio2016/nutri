d902ee3766f7e42bb96a59c304bc686e38e2d17d
Marcio2016 <marcio.tadeu2@gmail.com>
2020-05-14 21:24:27 -0300

    Primeiro commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component,ViewChild } from '@angular/core';
+import { NavController,ToastController } from 'ionic-angular';
+import { viewClassName } from '@angular/compiler';
+
+import { DicasPage } from './../dicas/dicas';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,22 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  @ViewChild('usuario') email;
+  @ViewChild('senha') password;
+  constructor(public navCtrl: NavController, public toastCtrl: ToastController) {
 
+  }
+
+  entrar(){
+    let message = this.toastCtrl.create({duration: 3000, position: 'bottom'});
+    if(this.email.value === "admin@admin.com.br" && this.password.value === 'admin'){
+      this.navCtrl.push(DicasPage);
+      message.setMessage('Seja Bem vindo!');
+      message.present();
+    }else{
+      message.setMessage('Usuário ou senha inválida!');
+      message.present();
+    }
   }
 
 }
