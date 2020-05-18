48abba22e9f863e6feece8851a568ccecac13cfa
Marcio <marcio@localhost.localdomain>
2020-05-18 17:09:03 -0300

    Implementando começando pagina de perfil

--- src/pages/dicas/dicas.ts
+++ src/pages/dicas/dicas.ts
@@ -13,11 +13,25 @@ import { HomePage } from './../home/home';
 export class DicasPage {
 
   email: string;
+  fotoPerfil: boolean = false;
+
+  facebook = {
+    nome: '',
+    fotoUrl: ''
+  }
 
   constructor(public navCtrl: NavController, public navParams: NavParams,
     public fire: AngularFireAuth, public toast: ToastController) {
 
       this.email = fire.auth.currentUser.email;
+      this.facebook.nome = fire.auth.currentUser.displayName;
+      this.facebook.fotoUrl = fire.auth.currentUser.photoURL;
+
+      if(this.facebook.fotoUrl == null) {
+        this.fotoPerfil;
+      }else {
+        this.fotoPerfil = true;
+      }
 
   }
 
